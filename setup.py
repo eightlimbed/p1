52ca501348d084e7908177943ce158d0b7f75b63
Lee Gaines <lgaines@ceresimaging.net>
2019-09-15 16:44:52 -0700

    l

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 setup(
     name='p1', 
-    version='0.0.666',
+    version='0.0.667',
     description='A sample Python project',
     url='https://github.com/eightlimbed/p1',
     packages=['p1'],
